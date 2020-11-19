1cf02e94f17a66e05c8beb4c42fbac34b5404701
sky <1005035266@qq.com>
2020-11-19 13:42:16 +0800

    help zj test

--- src/main/java/hm/com/controller/ManagerController.java
+++ src/main/java/hm/com/controller/ManagerController.java
@@ -3,11 +3,13 @@ package hm.com.controller;
 import hm.com.bean.Manager;
 import hm.com.bean.ReturnMessage;
 import hm.com.service.ManagerService;
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author ：sky
@@ -18,11 +20,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @CrossOrigin(origins = "*")
+//localhost:8080/api/manager
 @RequestMapping("/manager")
 public class ManagerController {
 
 //    @Autowired
 //    ManagerService managerService;
+
+    //localhost:8080/manager/test
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
+    public ReturnMessage test(){
+
+        return ReturnMessage.success().add("name","张军").add("数据","想要的数据");
+    }
 
     //添加管理员
     @RequestMapping(value = "",method = RequestMethod.POST)
