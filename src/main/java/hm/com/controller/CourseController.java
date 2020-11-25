1a167d7dc7e4789f904c8a7f940d1b3b6bacc7c7
zxl <1941921600@qq.com>
2020-11-25 13:10:30 +0800

    添加课程

--- src/main/java/hm/com/controller/CourseController.java
+++ src/main/java/hm/com/controller/CourseController.java
@@ -1,25 +1,42 @@
 package hm.com.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import hm.com.bean.Course;
 import hm.com.bean.ReturnMessage;
 import hm.com.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @CrossOrigin("*")
 @RequestMapping("/course")
 public class CourseController {
-    @Autowired
+    @Autowired  //自动注入函数内容
     CourseService courseService;
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @RequestMapping(value = "",method = RequestMethod.GET)
     @ResponseBody
 
-    public ReturnMessage test(){
-
-        return ReturnMessage.success().add("function","ceshi");
+    public ReturnMessage test(@RequestParam(value="page", defaultValue = "1")   Integer page,
+                              @RequestParam(value = "limit",defaultValue = "100") Integer limit){
+            System.out.println(page);
+            System.out.println(limit);
+        PageHelper.startPage(page,limit);
+        List<Course> list = courseService.getAll();
+            PageInfo pageInfo = new PageInfo(list, 5);
+            return ReturnMessage.success().add("pageInfo",pageInfo);
+        //从数据库获取信息
+        //返回信息
     }
+    @RequestMapping(value = "",method = RequestMethod.POST)
+    @ResponseBody
+    public ReturnMessage addCourse(@RequestBody Course course)
+    {
+        courseService.addCourse(course);
+        return ReturnMessage.success();
+    }
+
 }
--- src/main/java/hm/com/service/CourseService.java
+++ src/main/java/hm/com/service/CourseService.java
@@ -1,4 +1,24 @@
 package hm.com.service;
 
+import hm.com.bean.Course;
+import hm.com.dao.CourseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class CourseService {
+    @Autowired
+    CourseMapper courseMapper;
+    public List<Course> getAll(){
+        //1.从数据库获取信息
+        List <Course> list = courseMapper.selectByExample(null);
+        //返回
+        return list;
+    }
+
+    public void addCourse(Course course) {
+        courseMapper.insertSelective(course);
+        return ;
+    }
 }
